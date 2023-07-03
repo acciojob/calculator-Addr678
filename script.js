5166906fae8466f9a70dec4be19b9c12790f6ba4
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-03 13:27:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your code here
+// Get the display input element
+const display = document.getElementById('input');
+
+// Get all the number buttons and add event listeners
+const numberButtons = document.querySelectorAll('.block');
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    display.value += button.textContent;
+  });
+});
+
+// Get the decimal button and add event listener
+const decimalButton = document.getElementById('dot');
+decimalButton.addEventListener('click', () => {
+  if (!display.value.includes('.')) {
+    display.value += decimalButton.textContent;
+  }
+});
+
+// Get all the operator buttons and add event listeners
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    display.value += button.textContent;
+  });
+});
+
+// Get the equal button and add event listener
+const equalButton = document.getElementById('ans');
+equalButton.addEventListener('click', () => {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+});
+
+// Get the clear button and add event listener
+const clearButton = document.getElementById('clr');
+clearButton.addEventListener('click', () => {
+  display.value = '';
+});
+
